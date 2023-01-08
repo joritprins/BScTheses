9eb8c45d57fd9f6ef02eb15739e8025f55ea271f
Your Name <you@example.com>
2023-01-08 16:38:38 +0100

    parsed results and produced plots

--- Code/Plots/mean.py
+++ Code/Plots/mean.py
@@ -8,15 +8,15 @@ import json
 import os
 from functions import filter_results
 
-x_ = np.arange(0, 55, 0.1)
-def plot(dir: str, file_names: str, runs: int):
+def plot(dir: str, file_names: str, runs: int, end: int):
+    x_ = np.arange(0, end, 0.1)
     # Open file containing all PID's and save it as string
     PID_string = open('{}/pids'.format(dir), 'r').read().split("END")
     # Convert string to python dictionary
     PID_dict = json.loads("{{{}}}".format(",".join(PID_string[:-1])))
     
     interp_client = []; interp_server = []; arr = []
-    for i in range(0,runs):
+    for i in range(1,runs+1):
         json_string = open('{}/{}_{}.json'.format(dir, file_names, i), 'r').read().replace('\n', '')
         arr = np.array(json.loads(json_string))
     
@@ -32,7 +32,7 @@ def plot(dir: str, file_names: str, runs: int):
     
     plt.figure(figsize=(10,5))
     plt.ylim(0, max(round(np.max(interp_client)), round(np.max(interp_server))))
-    plt.xlim(0, 55)
+    plt.xlim(0, end)
     plt.xlabel("Time (s)")
     plt.ylabel("Power consumption (Wh)")
     plt.title("Power usage after {} runs".format(runs))
@@ -40,30 +40,35 @@ def plot(dir: str, file_names: str, runs: int):
     plt.plot(x_, np.mean(interp_server, 0), label="Server, mean: {}Wh".format(round(np.mean(np.mean(interp_server, 0)), 3)))
     plt.legend()
     plt.savefig("Code/Plots/Means/{}_{}.png".format(os.path.basename(__file__).partition(".py")[0], file_names))
-    return (np.mean(interp_client, 0), np.mean(interp_server, 0))
+    return (x_, np.mean(interp_client, 0), np.mean(interp_server, 0))
 
-cheetah_resnet50_client, cheetah_resnet50_server = plot('Code/Plots/cheetah_resnet50_50', 'cheetah_resnet50', 51)
-cheetah_sqnet_client, cheetah_sqnet_server = plot('Code/Plots/cheetah_sqnet_50', 'cheetah_sqnet', 51)
-SCI_HE_sqnet_client, SCI_HE_sqnet_server = plot('Code/Plots/SCI_HE_sqnet_11', 'SCI_HE_sqnet', 11)
-# SCI_HE_resnet50_client, SCI_HE_resnet50_server = plot('Code/Plots/SCI_HE_resnet50_11', 'SCI_HE_resnet50', 11)
-SCI_HE_resnet50_client, SCI_HE_resnet50_server = [], []
+xcr, cheetah_resnet50_client, cheetah_resnet50_server = plot('Code/Plots/Results/cheetah_resnet50_50', 'cheetah_resnet50', runs=50, end=256)
+xcs, cheetah_sqnet_client, cheetah_sqnet_server = plot('Code/Plots/Results/cheetah_sqnet_50', 'cheetah_sqnet', runs=50, end=53)
+xss, SCI_HE_sqnet_client, SCI_HE_sqnet_server = plot('Code/Plots/Results/SCI_HE_sqnet_50', 'SCI_HE_sqnet', runs=50, end=82)
+xsr, SCI_HE_resnet50_client, SCI_HE_resnet50_server = plot('Code/Plots/Results/SCI_HE_resnet50_50', 'SCI_HE_resnet50', runs=50, end=560)
 
 # Plot means of client side
 plt.figure(figsize=(10,5))
-plt.ylim(0, max(
-                max((np.max(cheetah_resnet50_client), np.max(cheetah_sqnet_client))),
-                # max((np.max(SCI_HE_resnet50_client), np.max(SCI_HE_sqnet_client)))))
-                np.max(SCI_HE_sqnet_server)))
-plt.xlim(0, 55)
+# plt.ylim(0, max(
+#                 max((np.max(cheetah_resnet50_client), np.max(cheetah_sqnet_client))),
+#                 # max((np.max(SCI_HE_resnet50_client), np.max(SCI_HE_sqnet_client)))))
+#                 np.max(SCI_HE_sqnet_server)))
+# plt.xlim(0, 55)
 plt.xlabel("Time (s)")
 plt.ylabel("Power consumption (Wh)")
-plt.title("Power usage of client")
-plt.plot(x_, cheetah_resnet50_client, label="Cheetah resnet50")
-plt.plot(x_, cheetah_sqnet_client, label="Cheetah sqnet")
-plt.plot(x_, SCI_HE_sqnet_client, label="SCI_HE resnet50")
-# plt.plot(x_, SCI_HE_resnet50_client, label="SCI_HE sqnet")
+plt.title("Power usage of client while running sqnet")
+plt.plot(xcs, cheetah_sqnet_client, 
+            label="Cheetah, mean: {}Wh, total: {}".format(
+                round(np.mean(cheetah_sqnet_client), 1), 
+                round(np.sum(cheetah_sqnet_client)*56/3600, 1)))
+plt.plot(xss, SCI_HE_sqnet_client, 
+            label="SCI_HE, mean: {}Wh, total: {}".format(
+                round(np.mean(SCI_HE_sqnet_client), 1), 
+                round(np.sum(SCI_HE_sqnet_client)*83/3600, 1)))
 plt.legend()
-plt.savefig("Code/Plots/Means/{}_client.png".format(os.path.basename(__file__).partition(".py")[0]))
+plt.savefig("Code/Plots/Means/{}_client_resnet.png".format(os.path.basename(__file__).partition(".py")[0]))
+
+exit()
 
 # Plot means of server side
 plt.figure(figsize=(10,5))
