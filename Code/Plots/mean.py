2fd62709705201fc1df5cf8db284450c03549225
Your Name <you@example.com>
2023-01-26 20:30:23 +0100

    updated means and names

--- Code/Plots/mean.py
+++ Code/Plots/mean.py
@@ -1,6 +1,7 @@
 # Insert path for functions.py file
 import sys
 sys.path.insert(0, '{}/..'.format(sys.path[0]))
+sys.path.insert(0, '{}/../..'.format(sys.path[0]))
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +10,17 @@ import os
 from functions import filter_results
 
 def plot(dir: str, file_names: str, runs: int, end: int):
-    x_ = np.arange(0, end, 1)
+    """
+    Plot the file in dir with name file_names
+
+    dir         : name of the dir that the files are in
+    file_names  : base of the file names
+    runs        : determines how many files there are -> files go from 1 till runs+1
+    end         : this script will interpolate the results from 0 till end
+
+    returns the interpolated client and server results
+    """
+    x_ = np.arange(0, end, .1)
     # Open file containing all PID's and save it as string
     PID_string = open('{}/pids'.format(dir), 'r').read().split("END")
     # Convert string to python dictionary
@@ -17,9 +28,11 @@ def plot(dir: str, file_names: str, runs: int, end: int):
     
     interp_client = []; interp_server = []; arr = []
     for i in range(1,runs+1):
-        json_string = open('{}/{}_{}.json'.format(dir, file_names, i), 'r').read().replace('\n', '')
+        # Readd files and convert to numpy array
+        json_string = open('{}/{}-{}.json'.format(dir, file_names, i), 'r').read().replace('\n', '')
         arr = np.array(json.loads(json_string))
-    
+
+        # Filter results
         start = arr[0]['host']['timestamp']
         filtered_client = filter_results(arr, PID_dict[str(i)]['client'], start=start)
         filtered_server = filter_results(arr, PID_dict[str(i)]['server'], start=start)
@@ -35,7 +48,7 @@ def plot(dir: str, file_names: str, runs: int, end: int):
     plt.xlim(0, end)
     plt.xlabel("Time (s)")
     plt.ylabel("Power consumption (W)")
-    data = file_names.split('_')
+    data = file_names.split('-')
     plt.title("Power usage of {} with {} (mean of {} runs)".format(data[0], data[1], runs))
     plt.plot(x_, np.mean(interp_client, 0), label="Client, mean: {}W, total: {}Wh".format(
                             round(np.mean(np.mean(interp_client, 0)), 3),
@@ -47,10 +60,10 @@ def plot(dir: str, file_names: str, runs: int, end: int):
     plt.savefig("Code/Plots/Means/{}_{}.png".format(os.path.basename(__file__).partition(".py")[0], file_names))
     return (x_, np.mean(interp_client, 0), np.mean(interp_server, 0))
 
-xcr, cheetah_resnet50_client, cheetah_resnet50_server = plot('Code/Plots/Results/same_device/cheetah_resnet50_50', 'cheetah_resnet50', runs=50, end=256)
-xcs, cheetah_sqnet_client, cheetah_sqnet_server = plot('Code/Plots/Results/same_device/cheetah_sqnet_50', 'cheetah_sqnet', runs=50, end=53)
-xss, SCI_HE_sqnet_client, SCI_HE_sqnet_server = plot('Code/Plots/Results/same_device/SCI_HE_sqnet_50', 'SCI_HE_sqnet', runs=50, end=82)
-xsr, SCI_HE_resnet50_client, SCI_HE_resnet50_server = plot('Code/Plots/Results/same_device/SCI_HE_resnet50_50', 'SCI_HE_resnet50', runs=50, end=560)
+xcr, cheetah_resnet50_client, cheetah_resnet50_server = plot('Code/Plots/Results/same-device/cheetah-resnet50-50', 'cheetah-resnet50', runs=50, end=256)
+xcs, cheetah_sqnet_client, cheetah_sqnet_server = plot('Code/Plots/Results/same-device/cheetah-sqnet-50', 'cheetah-sqnet', runs=50, end=53)
+xss, SCI_HE_sqnet_client, SCI_HE_sqnet_server = plot('Code/Plots/Results/same-device/SCI_HE-sqnet-50', 'SCI_HE-sqnet', runs=50, end=82)
+xsr, SCI_HE_resnet50_client, SCI_HE_resnet50_server = plot('Code/Plots/Results/same-device/SCI_HE-resnet50-50', 'SCI_HE-resnet50', runs=50, end=560)
 
 # Plot means of client side
 plt.figure(figsize=(10,5))
@@ -67,7 +80,7 @@ plt.plot(xcs, cheetah_sqnet_client,
                 round(np.mean(cheetah_sqnet_client), 1), 
                 round(np.sum(cheetah_sqnet_client)*56/3600, 1)))
 plt.plot(xss, SCI_HE_sqnet_client, 
-            label="SCI_HE, mean: {}Wh, total: {}W".format(
+            label="SCI_HE, mean: {}W, total: {}Wh".format(
                 round(np.mean(SCI_HE_sqnet_client), 1), 
                 round(np.sum(SCI_HE_sqnet_client)*83/3600, 1)))
 plt.legend()
